b7c53df2856effbfeacf70946e56346fa7d546b7
Dielder <dieldergabrieleal@hotmail.com>
2023-09-15 17:10:24 -0300

    Username verification

--- scripts.js
+++ scripts.js
@@ -5,5 +5,38 @@ const password = document.getElementById("password");
 const passwordConfirmation = document.getElementById("password-confirmation");
 
 form.addEventListener("submit", (e) => {
-   e.preventDefault();
+    e.preventDefault();
+
+    checkInputs();
 });
+
+function checkInputs() {
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    const passwordConfirmationValue = passwordConfirmation.value;
+
+    if (usernameValue === "") {
+        setErrorFor(username, "O nome de usuário é obrigatório.");
+    } else {
+        setSuccessFor(username);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+
+    // Adiciona a mensagem de erro
+    small.innerText = message;
+
+    // Adiciona a classe de erro
+    formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    // Adicionar a classe de sucesso
+    formControl.className = "form-control success";
+}
